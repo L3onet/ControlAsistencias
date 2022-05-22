bc15a4d62d140bad860d91a333f47c7f6692687a
Leonel Gonzalez Vidales <leonelgv@live.com>
2022-05-22 18:10:22 -0500

    Agregar archivos de proyecto.

---
+++ presentacion/MenuPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlAsistencias.presentacion
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblAutor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
